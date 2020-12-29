16eaeec8b811124dc5862c6f74e0776f848129f9
叶子 <2693830285@qq.com>
2020-12-29 14:59:47 +0800

    Initial commit

---
+++ office_main/src/main/java/com/yezi/office/OfficeApplication.java
@@ -0,0 +1,22 @@
+package com.yezi.office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 叶子
+ * @Description 主类
+ * @PackageName com.yezi.office
+ * @DevelopmentTools IntelliJ IDEA
+ * @Data 2020/12/29 星期二 14:32
+ */
+@SpringBootApplication
+public class OfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                OfficeApplication.class,
+                args
+        );
+    }
+}
