48382939d9bc3d8e9a5f7e851c5b2b2c8d84049c
叶子 <2693830285@qq.com>
2020-12-30 17:58:13 +0800

    init.sql

---
+++ init.sql
@@ -0,0 +1,28 @@
+# 初始数据
+
+insert into office_department
+values
+       ('9a47cda467f540b3b974aab20ed26e5d','人事部','管理人员调动',5,'1315356783'),
+       ('482bade3f42e4468baace8f97e0f6855','策划部','负责项目策划',5,'1315356783'),
+       ('910e3730f2d040d1bc1c644b72de8422','营销部','负责市场营销',5,'1315356783'),
+       ('8c857e0ab9dc40508ed71e3cd18efbf2','研发部','负责技术开发',5,'1315356783'),
+       ('debdeda133ca4cdbb9ed3034ee314e32','调研部','负责市场调研',5,'1315356783');
+
+# 初始化
+insert into office_user
+values
+('bb2e34c446074ca1908f5e06ba2db27a','宰腰呈','827ccb0eea8a706c4c34a16891f84e7b','https://office-yezi.oss-cn-beijing.aliyuncs.com/avator/avatar.jpg','afdewf',1,'河南省郑州市','294187139','19384721458','294187139@qq.com','9a47cda467f540b3b974aab20ed26e5d','2020-12-30 11:11:11'),
+('c2591a8bd8fe4001a34fcc8e9f8bf336','羿烧陕','827ccb0eea8a706c4c34a16891f84e7b','https://office-yezi.oss-cn-beijing.aliyuncs.com/avator/avatar.jpg','afdewf',1,'河南省郑州市','294187139','19384721458','294187139@qq.com','9a47cda467f540b3b974aab20ed26e5d','2020-12-30 11:11:11'),
+('6f5570153a3944ecaa90506f2a90c4ea','马处集','827ccb0eea8a706c4c34a16891f84e7b','https://office-yezi.oss-cn-beijing.aliyuncs.com/avator/avatar.jpg','afdewf',1,'河南省郑州市','294187139','19384721458','294187139@qq.com','9a47cda467f540b3b974aab20ed26e5d','2020-12-30 11:11:11'),
+('41b69629a6294077a71150d1a2912de3','盖撒汽','827ccb0eea8a706c4c34a16891f84e7b','https://office-yezi.oss-cn-beijing.aliyuncs.com/avator/avatar.jpg','afdewf',1,'河南省郑州市','294187139','19384721458','294187139@qq.com','482bade3f42e4468baace8f97e0f6855','2020-12-30 11:11:11'),
+('4ad501ed57c24fe9aa4afe996fbe3813','萧鸟缠','827ccb0eea8a706c4c34a16891f84e7b','https://office-yezi.oss-cn-beijing.aliyuncs.com/avator/avatar.jpg','afdewf',1,'河南省郑州市','294187139','19384721458','294187139@qq.com','482bade3f42e4468baace8f97e0f6855','2020-12-30 11:11:11'),
+('755349e23e2a4c2f9d91198a8a29c193','郗到生','827ccb0eea8a706c4c34a16891f84e7b','https://office-yezi.oss-cn-beijing.aliyuncs.com/avator/avatar.jpg','afdewf',1,'河南省郑州市','294187139','19384721458','294187139@qq.com','482bade3f42e4468baace8f97e0f6855','2020-12-30 11:11:11'),
+('884efda0fafa4bb8831cfcebea30bb13','薄进阁','827ccb0eea8a706c4c34a16891f84e7b','https://office-yezi.oss-cn-beijing.aliyuncs.com/avator/avatar.jpg','afdewf',1,'河南省郑州市','294187139','19384721458','294187139@qq.com','910e3730f2d040d1bc1c644b72de8422','2020-12-30 11:11:11'),
+('65bbfd96d3bc41eaa9d438252edb2b15','臧叔揉','827ccb0eea8a706c4c34a16891f84e7b','https://office-yezi.oss-cn-beijing.aliyuncs.com/avator/avatar.jpg','afdewf',1,'河南省郑州市','294187139','19384721458','294187139@qq.com','910e3730f2d040d1bc1c644b72de8422','2020-12-30 11:11:11'),
+('d5e13822383a408d8625f42c3dfdfecc','满佳耗','827ccb0eea8a706c4c34a16891f84e7b','https://office-yezi.oss-cn-beijing.aliyuncs.com/avator/avatar.jpg','afdewf',1,'河南省郑州市','294187139','19384721458','294187139@qq.com','910e3730f2d040d1bc1c644b72de8422','2020-12-30 11:11:11'),
+('19c00a33200649a7bba7369e4382a574','印晓智','827ccb0eea8a706c4c34a16891f84e7b','https://office-yezi.oss-cn-beijing.aliyuncs.com/avator/avatar.jpg','afdewf',1,'河南省郑州市','294187139','19384721458','294187139@qq.com','8c857e0ab9dc40508ed71e3cd18efbf2','2020-12-30 11:11:11'),
+('0cc70bb29e0541a7a2a261404e7adec5','全考语','827ccb0eea8a706c4c34a16891f84e7b','https://office-yezi.oss-cn-beijing.aliyuncs.com/avator/avatar.jpg','afdewf',1,'河南省郑州市','294187139','19384721458','294187139@qq.com','8c857e0ab9dc40508ed71e3cd18efbf2','2020-12-30 11:11:11'),
+('56a393abade544c79e806c0b7d129612','高彩尺','827ccb0eea8a706c4c34a16891f84e7b','https://office-yezi.oss-cn-beijing.aliyuncs.com/avator/avatar.jpg','afdewf',1,'河南省郑州市','294187139','19384721458','294187139@qq.com','8c857e0ab9dc40508ed71e3cd18efbf2','2020-12-30 11:11:11'),
+('6e5966cb730d459db28df89858867a99','曾滤疲','827ccb0eea8a706c4c34a16891f84e7b','https://office-yezi.oss-cn-beijing.aliyuncs.com/avator/avatar.jpg','afdewf',1,'河南省郑州市','294187139','19384721458','294187139@qq.com','debdeda133ca4cdbb9ed3034ee314e32','2020-12-30 11:11:11'),
+('c21286b9b7d147f0bf42ac8d8f26951f','狄传繁','827ccb0eea8a706c4c34a16891f84e7b','https://office-yezi.oss-cn-beijing.aliyuncs.com/avator/avatar.jpg','afdewf',1,'河南省郑州市','294187139','19384721458','294187139@qq.com','debdeda133ca4cdbb9ed3034ee314e32','2020-12-30 11:11:11'),
+('e8f10cfc51c44370bc86de547fd872c2','通旋部','827ccb0eea8a706c4c34a16891f84e7b','https://office-yezi.oss-cn-beijing.aliyuncs.com/avator/avatar.jpg','afdewf',1,'河南省郑州市','294187139','19384721458','294187139@qq.com','debdeda133ca4cdbb9ed3034ee314e32','2020-12-30 11:11:11');
